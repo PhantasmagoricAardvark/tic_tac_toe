c87d169fdb1b65e1fcf73afabfd2924f6c88cd29
seba <villeaho@tutanota.com>
2018-08-21 23:16:40 +0300

    Added board class with @@board. Also def board which returns the current board. The player Class can now place an x on the board.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,3 +1,27 @@
+class Board
+	@@board = "1|2|3\n4|5|6\n7|8|9"
 
-board = "1|2|3\n4|5|6\n7|8|9"
-puts board.sub("1", "x")
+	def board
+		puts ""
+		return @@board
+	end	
+
+	def self.place_x(num)
+		@@board.sub!(num.to_s, "x")
+	end
+end
+
+class Player
+	def choose(num)
+		Board.place_x(num)
+	end	
+end
+
+game = Board.new
+player = Player.new
+puts game.board
+puts game.board
+player.choose(3)
+puts game.board
+
+
