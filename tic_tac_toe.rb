692e15e91789d847183579ff754c706602842ffd
seba <villeaho@tutanota.com>
2018-08-23 22:02:05 +0300

    Created two victory methods for player and computer. They utilize the array of choices that the user has made (Computer or player), and then it is going to be checked with an array of victory conditions. The moderator class initiates everything and checks if the victory condition returns true to end the game, and it also checks if the options array is empty to call the game a draw.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -9,14 +9,6 @@ class Board
 		@@options
 	end
 
-	def self.x
-		@@x_s
-	end
-
-	def self.o
-		@@o_s
-	end
-
 	def board
 		puts ""
 		return @@board
@@ -34,25 +26,49 @@ class Board
 		@@board.sub!(num.to_s, "o")
 	end
 
-	def victory
-		i = 0
-		while i < 1
+	def self.player_victory
 			@@victories.each { |el|
-				
+				i = 0
+				counter = 0
+				while i < 3
+					if @@x_s.include?(el[i])
+						counter += 1
+					end
+					i += 1
+					if counter == 3
+						return true
+					end
+				end
 			}
-			i += 1
-		end
+			false
 	end	
+
+	def self.computer_victory
+			@@victories.each { |el|
+				i = 0
+				counter = 0
+				while i < 3
+					if @@o_s.include?(el[i])
+						counter += 1
+					end
+					i += 1
+					if counter == 3
+						return true
+					end
+				end
+			}
+			false
+	end	
+
 end
 
 class Player
 	def choose
-		puts
 		puts "Make your choice."
 		num = gets.chomp.to_i
 		until Board.options.include?(num)
 			puts Board.options.include?(num)
-			puts "That number has already been taken."
+			puts "That spot has already been taken."
 			num = gets.chomp.to_i
 		end
 		Board.place_x(num)
@@ -72,24 +88,42 @@ class Computer
 end
 
 class Moderator
-	def play_game
-		
+	def self.play_game
+		puts "Tic. Tac. Toe!"
+		puts "You are the x's."
+		puts "The computer plays with the o's."
+		puts "To make your choice type a number."
+		game = Board.new
+		player = Player.new
+		computer = Computer.new
+		puts "The board:"
+		puts game.board
+		while 1
+			puts
+			player.choose
+			if Board.player_victory
+				puts game.board
+				puts
+				puts "You win!"
+				break
+			elsif Board.options.length == 0
+				puts
+				puts game.board
+				puts "It's a draw!"
+				break
+			end
+			computer.choose
+			puts game.board
+			if Board.computer_victory
+				puts
+				puts "Computer wins!"
+				break
+			end
+		end
 	end
 end
 
-
-game = Board.new
-player = Player.new
-computer = Computer.new
-puts game.board
-computer.choose
-puts game.board
-player.choose
-puts game.board
-p Board.options
-p "x's" + Board.x.to_s
-p "o's" + Board.o.to_s
-
+Moderator.play_game
 
 
 
